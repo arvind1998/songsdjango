db0fd1aa06753becc2802fb380195d4d0056203b
arvind1998 <arvindramadurai7@gmail.com>
2017-06-29 20:00:45 +0530

    Second Commit

--- music/views.py
+++ music/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Album
 
@@ -9,10 +11,10 @@ def index(request):
 	return render(request, 'music/index.html', {'var_album':all_albums})
 
 def detail(request, album_id):
+	try:
+		album=Album.objects.get(pk=album_id)
+		return render(request, 'music/detail.html',{'album':album,})
 
-	get_object_or_404(Album, pk=album_id)
-
-	return render(request, 'music/detail.html',{'album':album,})
-
-
+	except Album.DoesNotExist:
+		raise Http404("Error 404: Album does not exist.")
 	
